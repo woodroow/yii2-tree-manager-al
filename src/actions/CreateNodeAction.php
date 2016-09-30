d6abdb80f854ba7f06a77f35135c691b4b716966
Вешкуров Артем <veshkurov.a.g@saturn-r.ru>
2016-09-30 11:16:22 +0500

    29.09.2016 // Adjacency List

--- src/actions/CreateNodeAction.php
+++ src/actions/CreateNodeAction.php
@@ -34,8 +34,8 @@ class CreateNodeAction extends BaseAction
 
         if (isset($roots[0])) {
             //$name = Yii::$app->request->post('StructureAL[name]');
-            print_r($params['StructureAL']);
-            //$model->name = $params->name;
+            //print_r($params['StructureAL']['name']);
+            $model->name = $params['StructureAL']['name'];
             return $model->appendTo($roots[0])->save();
         } else {
             return $model->makeRoot()->save();
